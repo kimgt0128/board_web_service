ed7c56abc3b09b06d0649f6a9d82ef7fde477225
KKT <kimgt2828@gmail.com>
2024-10-18 23:30:16 +0900

    refactor: 프로젝트 설정파일 - H2 설정 - 데이터베이스 설정 - SQL로 더미데이터 추가 - 로깅 레벨 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+-- article 더미 데이터
+INSERT INTO posts(title, content, author) VALUES('삼성바이오로직스', '100주 매수', '홍길동');
+INSERT INTO posts(title, content, author) VALUES('엔비디아', '200주 매수', '김길동');
+INSERT INTO posts(title, content, author) VALUES('구글', '300주 매수', '박길동');
