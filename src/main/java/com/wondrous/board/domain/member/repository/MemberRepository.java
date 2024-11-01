dadceb320eda4cf887d18aad8432013797f48d35
KKT <kimgt2828@gmail.com>
2024-11-02 00:02:40 +0900

    feat: OAuth2 관련 DTO들 생성 - OAuth2Response : Naver, Kakao, Google Response의 인터페이스 - CustomOAuth2User : 입력받은 소셜 로그인을 통합한 최종 MemberDTO

--- src/main/java/com/wondrous/board/domain/member/repository/MemberRepository.java
+++ src/main/java/com/wondrous/board/domain/member/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package com.wondrous.board.domain.member.repository;
 
 import com.wondrous.board.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -9,5 +10,4 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     //이미 가입된 사용자인지 확인하기 위함
     Optional<Member> findByEmail(String email);
-
 }
